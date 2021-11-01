535c958aa4615224459cbac4288ad7ef73a587ee
Sven Lohrmann <malnvenshorn@mailbox.org>
2021-11-01 18:09:13 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hello_world',
+    version='0.1',
+    description='say hello in python',
+    url='https://github.com/malnvenshorn/python-hello-world',
+    author='malnvenshorn',
+    author_email='malnvenshorn@mailbox.org',
+    license='GPLv3',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+)
